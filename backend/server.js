b07365bddd67fe3699f06763e25e7bde99cb1d14
Vvvvvaleriia <reznikvaleria31@gmail.com>
2026-03-18 11:52:24 +0200

    Add backend folder to work

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const http = require("http");
+
+const clientId = "12304fbfa93a45008be04110a623ca46"
+const clientSecret = "78456bf94cac4917abcaf3c973b631e3"
+const redirectUrl = "http://127.0.0.1:5500/musicapp/"
+
+const server = http.createServer((req, resp) => {
+    resp.setHeader("Access-Control-Allow-Origin", "*")
+    resp.setHeader("Access-Control-Allow-Methodds", "GET, POST, OPTIONS")
+    resp.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+    if (req.method === "OPTIONS"){
+        resp.writeHead(200, {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methodds": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type"
+        })
+        resp.end()
+        return
+    }
+
+    if (req.method === "POST"){
+        if (req.url === "/api/access"){
+            let body = ''
+            req.on("data", (chunk) => {
+                body += chunk
+            })
+
+            req.on("end", () => {
+                const data = JSON.parse(body)
+
+                fetch("https://accounts.spotify.com/api/token", {
+                    method: "POST",
+                    headers: {
+                    'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+                    'Content-Type' : 'application/x-www-form-urlencoded'
+                    },
+                    body: new URLSearchParams({
+                    code: data.code,
+                    redirect_uri: redirectUrl,
+                    grant_type: 'authorization_code'
+                    })
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    resp.writeHead(200, {"Content-Type": "application/json"})
+                    resp.end(JSON.stringify({"token": data.access_token}))
+                })
+            })
+            return
+        }
+    }
+    resp.writeHead(404);
+    resp.end("Not Found")
+})
