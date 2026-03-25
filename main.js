7ba9ae70cb67b99c27ede1987d6648f1bf730cd6
Vvvvvaleriia <reznikvaleria31@gmail.com>
2026-03-25 20:40:01 +0200

    Changed the architecture of functions

--- main.js
+++ main.js
@@ -18,22 +18,112 @@ let trackId;
 
 window.onSpotifyWebPlaybackSDKReady = () => {};
 
-musicTest.onclick = async () => {
-	async function getTrack() {
-		const resp = await fetch(
-			`https://api.spotify.com/v1/tracks/${trackId}`,
-			{
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-				method: "GET",
+//AUTHORIZE
+async function authorize(code) {
+	const response = await fetch("http://127.0.0.1:3000/api/access", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ code }),
+	});
+
+	const data = await response.json();
+	token = data.token;
+}
+
+//SEARCH
+async function searchForItem(token, textInput, type) {
+	const resp = await fetch(
+		`https://api.spotify.com/v1/search?q=${textInput}&type=${type}`,
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
 			},
-		);
+			method: "GET",
+		},
+	);
 
-		const json = await resp.json();
-		return json.uri;
-	}
+	const json = await resp.json();
+	return json;
+}
 
+//GET TRACK
+async function getTrack() {
+	const resp = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		method: "GET",
+	});
+
+	const json = await resp.json();
+	return json.uri;
+}
+
+//INIT PLAYER
+async function initSpotifyPlayer() {
+	player = new Spotify.Player({
+		name: "Web Playback SDK Quick Start Player",
+		getOAuthToken: (cb) => {
+			cb(token);
+		},
+		volume: 0.5,
+	});
+
+	player.addListener(
+		"player_state_changed",
+		({ track_window: { current_track } }) => {
+			trackName.innerText = current_track.name;
+			trackArtist.innerText = current_track.artists[0].name;
+		},
+	);
+
+	player.addListener("ready", ({ device_id }) => {
+		console.log("Ready with Device ID", device_id);
+		deviceId = device_id;
+	});
+
+	player.connect();
+
+	toggleBtn.onclick = () => {
+		player.togglePlay().then(() => {
+			console.log("Toggled playback");
+		});
+	};
+
+	volumeUp.onclick = () => {
+		player.getVolume().then((volume) => {
+			player.setVolume(Math.min(volume + 0.1, 1));
+		});
+	};
+
+	volumeDown.onclick = () => {
+		player.getVolume().then((volume) => {
+			player.setVolume(Math.max(volume - 0.1, 0));
+		});
+	};
+}
+
+//EVENTS
+btnLogin.onclick = () => {
+	window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&scope=streaming user-read-email user-read-private user-modify-playback-state&redirect_uri=${encodeURIComponent(redirectUrl)}`;
+};
+
+searchBtn.onclick = async () => {
+	const input = document.querySelector(".name-inp");
+	const textInput = input.value;
+
+	const result = await searchForItem(token, textInput, "track");
+	const track = result.tracks.items[0];
+	trackName.innerText = track.name;
+	trackArtist.innerText = track.artists[0].name;
+
+	trackId = track.id;
+	console.log(trackId);
+};
+
+musicTest.onclick = async () => {
 	const uri = await getTrack();
 
 	fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
@@ -48,101 +138,13 @@ musicTest.onclick = async () => {
 	});
 };
 
-btnLogin.onclick = () => {
-	window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&scope=streaming user-read-email user-read-private user-modify-playback-state&redirect_uri=${encodeURIComponent(redirectUrl)}`;
-};
+// START APP
+async function startApp() {
+	const code = window.location.search.replace("?code=", "");
 
-const code = window.location.search.replace("?code=", "");
+	if (!code) return;
 
-if (code) {
-	async function startApp() {
-		async function authorize() {
-			const response = await fetch("http://127.0.0.1:3000/api/access", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ code }),
-			});
-
-			const data = await response.json();
-			token = data.token;
-		}
-
-		await authorize();
-
-		// Search
-		searchBtn.onclick = async () => {
-			const input = document.querySelector(".name-inp");
-			const textInput = input.value;
-
-			async function searchForItem(token, textInput, type) {
-				const resp = await fetch(
-					`https://api.spotify.com/v1/search?q=${textInput}&type=${type}`,
-					{
-						headers: {
-							Authorization: `Bearer ${token}`,
-						},
-						method: "GET",
-					},
-				);
-
-				const json = await resp.json();
-				return json;
-			}
-
-			const result = await searchForItem(token, textInput, "track");
-			const track = result.tracks.items[0];
-			trackName.innerText = track.name;
-			trackArtist.innerText = track.artists[0].name;
-
-			trackId = track.id;
-			console.log(trackId);
-		};
-
-		async function initSpotifyPlayer() {
-			player = new Spotify.Player({
-				name: "Web Playback SDK Quick Start Player",
-				getOAuthToken: (cb) => {
-					cb(token);
-				},
-				volume: 0.5,
-			});
-
-			player.addListener(
-				"player_state_changed",
-				({ track_window: { current_track } }) => {
-					trackName.innerText = current_track.name;
-					trackArtist.innerText = current_track.artists[0].name;
-				},
-			);
-
-			player.addListener("ready", ({ device_id }) => {
-				console.log("Ready with Device ID", device_id);
-				deviceId = device_id;
-			});
-
-			player.connect();
-
-			toggleBtn.onclick = () => {
-				player.togglePlay().then(() => {
-					console.log("Toggled playback");
-				});
-			};
-
-			volumeUp.onclick = () => {
-				player.getVolume().then((volume) => {
-					player.setVolume(Math.min(volume + 0.1, 1));
-				});
-			};
-
-			volumeDown.onclick = () => {
-				player.getVolume().then((volume) => {
-					player.setVolume(Math.max(volume - 0.1, 0));
-				});
-			};
-		}
-		initSpotifyPlayer();
-	}
-	startApp();
+	await authorize(code);
+	await initSpotifyPlayer();
 }
+startApp();
